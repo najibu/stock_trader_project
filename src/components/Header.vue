ccc8d3e18ddbbed1d8a4095c9980de07d9fe5822
najibu <ns.najibu@gmail.com>
2017-07-09 16:03:50 +0100

    Adding drop down

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,7 +14,11 @@
       <strong class="navbar-text navbar-right">Funds: {{ funds | currency}}</strong>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#" @click="endDay">End Day</a></li>
-        <li class="dropdown">
+        <li
+          class="dropdown"
+          :class="{open: isDropdownOpen}"
+          @click="isDropdownOpen = !isDropdownOpen"
+        >
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save & Load <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="#">Save Data</a></li>
@@ -31,6 +35,11 @@
   import { mapActions } from 'vuex';
 
   export default {
+    data () {
+      return {
+        isDropdownOpen: false
+      }
+    },
     computed: {
       funds () {
         return this.$store.getters.funds;
